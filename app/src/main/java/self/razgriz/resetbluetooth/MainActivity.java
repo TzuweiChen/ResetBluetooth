5120eaaccc41987541134999967165df24f7aecc
Tzuwei Chen <chen.tzuwei@gmail.com>
2017-10-20 14:47:50 +0800

    add behavior intent to Android Wear app after back from Bluetooth Setting page

--- app/src/main/java/self/razgriz/resetbluetooth/MainActivity.java
+++ app/src/main/java/self/razgriz/resetbluetooth/MainActivity.java
@@ -5,22 +5,40 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private final int REQUEST_CODE_BLUETOOTH_SETTING = 9487;
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String PACKAGE = "package";
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String PACKAGE_NAME_ANDROID_BLUETOOTH = "com.android.bluetooth";
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String PACKAGE_NAME_ANDROID_WEAR = "com.google.android.wearable.app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
 
         Intent intent = new Intent();
         intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.fromParts("package", "com.android.bluetooth", null));
-        startActivity(intent);
-        MainActivity.this.finish();
+        intent.setData(Uri.fromParts(PACKAGE, PACKAGE_NAME_ANDROID_BLUETOOTH, null));
+        startActivityForResult(intent, REQUEST_CODE_BLUETOOTH_SETTING);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_BLUETOOTH_SETTING) {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(PACKAGE_NAME_ANDROID_WEAR);
+            if (intent != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "Android Wear application not found", Toast.LENGTH_SHORT).show();
+            }
+            MainActivity.this.finish();
+        }
     }
 }
